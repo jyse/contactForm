c21289b96c8c83f4d7a7c775ea67f2dcaa9181ec
Florian Boehmak <florian@fnbk.cc>
2020-03-19 14:50:12 +0100

    fix php server

--- README.md
+++ README.md
@@ -1,12 +1,40 @@
 # Contact Form: React + PHP
 
-** start React**
+**start React**
 ```
 npm start
 ```
 
-** start PHP**
+**start PHP**
 ```
 php -S localhost:8000
 ```
+
+**Test with Browser**
+```
+visit http://localhost:3000
+```
+
+**Test with Postman**
+```
+GET http://localhost:8000
+
+
+OPTION http://localhost:8000
+
+
+POST http://localhost:8000
+Content-Type: application/json
+{
+	"fname":"Hello",
+	"lname":"World",
+	"email":"hello@world.com",
+	"message":"abc",
+	"mailSent":false,
+	"error":""
+}
+```
+
+
+
 
--- api/contact/index.php
+++ api/contact/index.php
@@ -1,45 +1,57 @@
 <?php
-header("Access-Control-Allow-Origin: http://localhost:3000");
-header("Content-Type: application/json");
 
-$rest_json = file_get_contents("php://input");
-$_POST = json_decode($rest_json, true);
+if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+	echo 'Hello World';
+	exit(0);
+}
 
-// if (empty($_POST['fname']) && empty($_POST['email'])) die();
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+	header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+	header("Access-Control-Allow-Headers: Authorization, Content-Type,Accept, Origin");
+	header("Access-Control-Allow-Origin: http://localhost:3000");
+	header("Content-Type: application/json");
+	exit(0);
+}
 
-if ($_POST)
-	{
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+	// header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+	// header("Access-Control-Allow-Headers: Authorization, Content-Type,Accept, Origin");
+	header("Content-Type: application/json");
 
-	// set response code - 200 OK
+	// parse payload
+	$rest_json = file_get_contents("php://input");
+	$payload = json_decode($rest_json, true);
 
-	http_response_code(200);
-	$subject = $_POST['fname'];
+	// build email
+	$subject = $payload['fname'];
 	$to = "jessythe@gmail.com";
-	$from = $_POST['email'];
-
-	// data
-
-	$msg = $_POST['number'] . $_POST['message'];
-
-	// Headers
-
+	$from = $payload['email'];
+	$msg =  $payload['message'];
 	$headers = "MIME-Version: 1.0\r\n";
 	$headers.= "Content-type: text/html; charset=UTF-8\r\n";
 	$headers.= "From: <" . $from . ">";
-	mail($to, $subject, $msg, $headers);
 
-	// echo json_encode( $_POST );
+	// if you don't have a local email server running, then the following line will not work:
+	// mail($to, $subject, $msg, $headers);
 
-	echojson_encode(array(
+	// log to debug console
+	error_log("");
+	error_log("--------------------------------------------");
+	error_log("headers: ");
+	error_log($headers);
+	error_log("subject: " . $subject);
+	error_log("to: " . $to);
+	error_log("from: " . $from);
+	error_log("msg: " . $msg);
+	error_log("--------------------------------------------");
+
+	// write response
+	http_response_code(200);
+	echo json_encode(array(
 		"sent" => true
 	));
-	}
-  else
-	{
-		echo 'Hello World';
-	// tell the user about error
 
-	// echojson_encode(["sent" => false, "message" => "Something went wrong"]);
-	}
+	exit(0);
+}
 
 ?>
